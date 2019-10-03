fd4d942ae86284211b9f0d60585ab340ef6c527e
Trinity Pointard <trinity.pointard@insa-rennes.fr>
2019-10-03 12:51:08 +0200

    add some dataclasses

--- ipset.py
+++ ipset.py
@@ -54,27 +54,17 @@ class Ipset:
         if not success:
             raise IpsetError(err)
 
-    def add(self, entry, timeout=None, packets=None, bytes=None, comment=None,
-            skbmark=None, skbprio=None, skbqueue=None, nomatch=False, exist=True):
-        args = [self.name, entry]
-        if timeout:
-            args += ["timeout", timeout]
-        if packets:
-            args += ["packets", packets]
-        if bytes:
-            args += ["bytes", bytes]
-        if comment:
-            args += ["comment", comment]
-        if skbmark:
-            args += ["skbmark", skbmark]
-        if skbprio:
-            args += ["skbprio", skbprio]
-        if skbqueue:
-            args += ["skbqueue", skbqueue]
+    def add(self, entry, nomatch=False, exist=True):
+        if type(entry) is Entry:
+            args = [self.name] + entry.to_cmd()
+        else:
+            args = [self.name, entry]
         if nomatch:
-            args += ["namatch"]
+            args += ["nomatch"]
         if exist:
             args += ["-exist"]
+
+        print(args)
 
         success, _, err = _run_cmd("add", args)
 
@@ -82,7 +72,10 @@ class Ipset:
             raise IpsetError(err)
 
     def delete(self, entry, exist=True):
-        args = [self.name, entry]
+        if type(entry) is Entry:
+            args = [self.name, entry.elem]
+        else:
+            args = [self.name, entry]
         if exist:
             args += ["-exist"]
 
@@ -92,7 +85,13 @@ class Ipset:
             raise IpsetError(err)
 
     def test(self, entry):
-        success, _, err = _run_cmd("del", [entry])
+        if type(entry) is Entry:
+            args = [entry.elem]
+        else:
+            args = [entry]
+
+        success, _, err = _run_cmd("del", args)
+
         if success:
             return True
         if "is NOT in set" in err:
@@ -100,7 +99,10 @@ class Ipset:
         raise IpsetError(err)
 
     def list(self):
-        pass
+        success, res, err = _run_cmd("list", [self.name])
+        if not success:
+            raise IpsetError(err)
+        return Set.from_dict(res["ipsets"]["ipset"])
 
     def flush(self):
         success, _, err = _run_cmd("flush", [self.name])
@@ -124,3 +126,67 @@ class Ipset:
 
     def real():
         return True
+
+
+class Set:
+    def __init__(self, name, typ, header, entries):
+        self.name = name #string
+        self.type = typ #string
+        self.header = header #dict
+        self.entries = entries #list of entries
+
+    def from_dict(data):
+        if data["members"] is None:
+            entries = []
+        elif type(data["members"]["member"]) is list:
+            entries = [Entry(**e) for e in data["members"]["member"]]
+        else:
+            entries = [Entry(**data["members"]["member"])]
+        return Set(data["@name"], data["type"], dict(data["header"]), entries)
+
+class Entry:
+    def __init__(self, elem, timeout=None, packets=None, bytes=None, comment=None, skbmark=None, skbprio=None, skbqueue=None):
+        self.elem = elem
+        self.comment = comment
+        self.timeout = int(timeout) if timeout is not None else None
+        self.packets = int(packets) if packets is not None else None
+        self.bytes = int(bytes) if bytes is not None else None
+        self.skbqueue = int(skbqueue) if skbqueue is not None else None
+        if skbmark is None:
+            self.sekbmark = None
+        elif type(skbmark) is int:
+            self.skbmark = (skbmark, 2**32-1)
+        elif type(skbmark) is tuple:
+            self.skbmark = skbmark
+        else:
+            if "/" in skbmark:
+                mark,mask = skbmark.split("/")
+                self.skbmark = (int(mark, 16), int(mask,16))
+            else:
+                self.skbmark = (int(skbmark, 16), 2**32-1)
+
+        if skbprio is None:
+            self.skbprio = None
+        elif type(skbprio) is tuple:
+            self.skbprio = skbprio
+        else:
+            maj,min = skbprio.split(":")
+            self.skbprio = (int(maj), int(min))
+
+    def to_cmd(self):
+        res = [self.elem]
+        if self.comment:
+            res += ["comment", self.comment]
+        if self.timeout:
+            res += ["timeout", str(self.timeout)]
+        if self.packets:
+            res += ["packets", str(self.packets)]
+        if self.bytes:
+            res += ["bytes", str(self.bytes)]
+        if self.skbqueue:
+            res += ["skbqueue", str(self.skbqueue)]
+        if self.skbmark:
+            res += ["skbmark", '0x{:x}/0x{:x}'.format(*self.skbmark)]
+        if self.skbprio:
+            res += ["skbprio", '{}:{}'.format(*self.skbprio)]
+        return res
